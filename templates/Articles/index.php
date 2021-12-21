e20b2b53fb676334da668fe31949e5253725b0fc
keimichdd <jan@deliverydireto.com.br>
2021-12-21 16:35:58 -0300

    Criando um Blog - Parte 2 - Done

---
+++ templates/Articles/index.php
@@ -0,0 +1,31 @@
+<h1>Blog articles</h1>
+<?= $this->Html->link('Adicionar artigo', ['action' => 'add']) ?>
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Title</th>
+        <th>Created</th>
+    </tr>
+
+    <!-- Aqui é onde iremos iterar nosso objeto de solicitação $articles, exibindo informações de artigos -->
+
+    <?php foreach ($articles as $article) : ?>
+        <tr>
+            <td><?= $article->id ?></td>
+            <td>
+                <?= $this->Html->link($article->title, ['action' => 'view', $article->id]) ?>
+            </td>
+            <td>
+                <?= $article->created->format(DATE_RFC850) ?>
+            </td>
+            <td>
+            <?= $this->Form->postLink(
+                'Deletar',
+                ['action' => 'delete', $article->id],
+                ['confirm' => 'Tem certeza?'])
+            ?>
+            <?= $this->Html->link('Edit', ['action' => 'edit', $article->id]) ?>
+        </td>
+        </tr>
+    <?php endforeach; ?>
+</table>
